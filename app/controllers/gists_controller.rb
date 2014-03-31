a28ff0eeb94a70ff84d89d93c402655cdc6e9875
klamkiewicz <icewind91@wp.pl>
2014-03-31 23:52:16 +0200

    Search

--- app/controllers/gists_controller.rb
+++ app/controllers/gists_controller.rb
@@ -4,7 +4,11 @@ class GistsController < ApplicationController
   # GET /gists
   # GET /gists.json
   def index
-    @gists = Gist.paginate(page: params[:page], per_page: 3)
+       @gists = Gist.paginate(:page => params[:page]).search(params[:search])
+			respond_to do |format|
+			format.html
+			format.js
+		end
   end
 
   # GET /gists/1
